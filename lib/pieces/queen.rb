196d7390e47a6be7ccdd55473c88517ba7419165
otherdcn <danny.c.ntwari@gmail.com>
2025-03-20 16:40:27 +0200

    lib: add queen chess piece with specs

---
+++ lib/pieces/queen.rb
@@ -0,0 +1,45 @@
+require_relative "./piece"
+
+module ChessPiece
+  class Queen < Piece
+    MOVES = {
+      left: [0, -1],
+      right: [0, 1],
+      up: [-1, 0],
+      down: [1, 0],
+      up_left: [-1, -1],
+      up_right: [-1, 1],
+      down_right: [1, 1],
+      down_left: [1, -1]
+    }.freeze
+
+    private
+
+    def generate_legal_squares(current_square)
+      squares = []
+
+      MOVES.each_value do |coord|
+        (1..7).each do |shift_factor|
+          new_coord = coord.map { |ele| ele * shift_factor }
+          new_square = shift_squares(new_coord, current_square)
+
+          if within_boundary?(new_square)
+            squares << new_square
+            #next
+          else
+            break
+          end
+        end
+      end
+
+      squares
+    end
+
+    def shift_squares(coord, current_square)
+      file = current_square[0] + coord[0]
+      rank = current_square[1] + coord[1]
+
+      [file, rank]
+    end
+  end
+end
---
+++ spec/pieces/queen_spec.rb
@@ -0,0 +1,26 @@
+require_relative "../../lib/pieces/queen"
+require_relative "../../lib/pieces/piece"
+require_relative "./piece_spec.rb"
+
+RSpec.describe ChessPiece::Queen do
+  context 'Base class methods' do
+    include_examples "can populate a graph"
+
+    reachable_test_input = {
+      "a1" => %w[a8 h8 a7 g7 a6 f6 a5 e5 a4 d4 a3 c3 a2 b2 b1 c1 d1 e1 f1 g1 h1],
+      "d4" => %w[d8 h8 a7 d7 g7 b6 d6 f6 c5 d5 e5 a4 b4 c4 d3 d2 d1 e4 f4 g4 h4 c3 b2 a1 e3 f2 g1],
+      "h8" => %w[a8 b8 c8 d8 e8 f8 g8 h7 h6 h5 h4 h3 h2 h1 g7 f6 e5 d4 c3 b2 a1],
+      validity: {valid: "a1", invalid: "z1"},
+      legality: { from: "a1", to: "b3" }
+    }
+
+    include_examples "can check squares reachability", reachable_test_input
+
+    next_moves_test_input = {
+      "a1" => %w[a8 h8 a7 g7 a6 f6 a5 e5 a4 d4 a3 c3 a2 b2 b1 c1 d1 e1 f1 g1 h1],
+      "d4" => %w[d8 h8 a7 d7 g7 b6 d6 f6 c5 d5 e5 a4 b4 c4 d3 d2 d1 e4 f4 g4 h4 c3 b2 a1 e3 f2 g1],
+      "h8" => %w[a8 b8 c8 d8 e8 f8 g8 h7 h6 h5 h4 h3 h2 h1 g7 f6 e5 d4 c3 b2 a1]
+    }
+    include_examples "can provide next moves", next_moves_test_input
+  end
+end
