a08edf066a4185df23a1ab7b5f66a7550acc5de8
otherdcn <danny.c.ntwari@gmail.com>
2025-03-31 20:00:24 +0200

    lib: edit rook MOVES constant
    Also edit the #generate_legal_squares and #shift_squares methods as to
    how to access the constant.

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -3,10 +3,10 @@ require_relative "./piece"
 module ChessPiece
   class Rook < Piece
     MOVES = {
-      left: [:file, -1],
-      right: [:file, 1],
-      up: [:rank, 1],
-      down: [:rank, -1]
+      left: [0, -1],
+      right: [0, 1],
+      up: [1, 0],
+      down: [-1, 0]
     }.freeze
 
     def initialize(colour: 'White', type: 'Rook')
@@ -18,12 +18,10 @@ module ChessPiece
     def generate_legal_squares(current_square)
       squares = []
 
-      MOVES.each_value do |coord| # first: -1
+      MOVES.each_value do |coord|
         (1..7).each do |shift_factor|
-          factor = coord[1] * shift_factor
-          position = coord[0]
-
-          new_square = shift_squares(position, factor, current_square)
+          new_coord = coord.map { |ele| ele * shift_factor }
+          new_square = shift_squares(new_coord, current_square)
 
           if within_boundary?(new_square)
             squares << new_square
@@ -36,16 +34,9 @@ module ChessPiece
       squares
     end
 
-    def shift_squares(position, shift_factor, current_square)
-      if position == :file
-        rank = current_square[0] + shift_factor
-        file = current_square[1]
-      end
-
-      if position == :rank
-        rank = current_square[0]
-        file = current_square[1] + shift_factor
-      end
+    def shift_squares(coord, current_square)
+      rank = current_square[0] + coord[0]
+      file = current_square[1] + coord[1]
 
       [rank, file]
     end
