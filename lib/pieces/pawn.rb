27fd675ae5f0d9343b5edb705540cc80ee8e5940
otherdcn <danny.c.ntwari@gmail.com>
2025-04-01 20:47:42 +0200

    lib: edit Pawn's graph while setting edges
    Only set the optional 2 forward square advancement for the second row
    of both white and black pawns.

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -19,16 +19,43 @@ module ChessPiece
       colour == "White" ? false : true
     end
 
-    def generate_legal_squares(current_square)
+    def add_possible_movements_per_board_positions(board)
+      board.each_with_index do |row, row_idx|
+        row.each_with_index do |col, col_idx|
+          square_id = col
+          square_coord = [row_idx, col_idx]
+
+          squares = generate_legal_squares(square_coord, row_idx)
+
+          squares.each do |target_coord|
+            target_rank, target_file = target_coord
+
+            target_id = board[target_rank][target_file]
+            add_square_to_movements(square_id, target_id)
+          end
+        end
+      end
+    end
+
+    def generate_legal_squares(current_square, row_idx)
       squares = []
 
       if reverse_movements
         moves = MOVES.transform_values { |value| value.map { |x| x * -1 } }
+        rank_idx = 1
       else
         moves = MOVES
+        rank_idx = 6
       end
       
-      moves.each_value do |coord|
+      two_up_square = add_two_square_advancement(moves[:_2_up],
+                      current_square) if row_idx == rank_idx
+
+      squares << two_up_square unless two_up_square.nil?
+
+      moves.each do |direction, coord|
+        next if direction == :_2_up
+
         new_square = shift_squares(coord, current_square)
 
         next unless within_boundary?(new_square)
@@ -37,6 +64,14 @@ module ChessPiece
       end
 
       squares
+    end
+
+    def add_two_square_advancement(coord, current_square)
+      new_square = shift_squares(coord, current_square)
+
+      return nil unless within_boundary?(new_square)
+
+      new_square
     end
 
     def shift_squares(coord, current_square)
